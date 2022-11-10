9379395e34086b27bbfee43434784340e891bd59
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-10 16:35:20 -0500

    Beginning adding JS by adding objects and selecting elements with getElementsByClassName

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("JS is running")
+
+const lifeOfPet = document.getElementsByClassName("life-of-pet-text")
+console.log(lifeOfPet)
+
+const petBio = document.getElementsByClassName("pet-bio")
+console.log(petBio)
+
+const ageCount = document.getElementsByClassName("age-count")
+console.log(ageCount)
+
+const petStatus = document.getElementsByClassName("pet-status")
+console.log(petStatus)
+
+const petName = document.getElementsByClassName("pet-name-input")
+console.log(petName)
+
+const boredomLevel = document.getElementsByClassName("boredom-level")
+console.log(boredomLevel)
+
+const hungerLevel = document.getElementsByClassName("hunger-level")
+console.log(hungerLevel)
+
+const sleepinessLevel = document.getElementsByClassName("sleepiness-level")
+console.log(sleepinessLevel)
