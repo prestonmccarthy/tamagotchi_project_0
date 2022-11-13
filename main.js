e8b0e673ba193f5ec2febe4d3cced8799147f603
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-12 22:41:54 -0500

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,56 +1,16 @@
 console.log("JS is running")
 
 const lifeOfPet = document.getElementsByClassName("life-of-pet-text")
-
-
 const petBio = document.getElementsByClassName("pet-bio")
-
-
 const ageCount = document.querySelector(".age-count")
-
-
 const petStatus = document.querySelector(".pet-status")
-
-
 const petName = document.getElementsByClassName("pet-name-input")
-
-
 const boredomLevel = document.querySelector(".boredom-level")
-
-
 const hungerLevel = document.querySelector(".hunger-level")
-
-
 const sleepinessLevel = document.querySelector(".sleepiness-level")
-
-
 const playBtn = document.getElementById("play-button")
-
-
 const hungerButton = document.querySelector(".feed-button")
-
-
 const sleepBtn = document.getElementById("nap-button")
-
-// function feedPet () {
-//     petFed *= 0;
-//     hungerLevel.innerHTML = `Hunger: ${petFed}`
-//     console.log(hungerLevel)
-//     // console.log(event)
-//     // //store the current text content of the p tag 
-//     // let display = parseInt(hungerLevel.textContent) // parseInt - convert a non integer value into an integer 
-//     // //parseInt("3") => 3
-//     // // console.log(display)
-//     // display-=1
-//     // hungerLevel.textContent = display
-//     // console.log(numberDisplay)
-//     // every time increaseNumber is called the value will be different - increaseNumber is a wrapper for some behavior
-//     // on click - change the dom from 0 > 1 > 2 > etc. 
-// } 
-
-// // hungerBtn.addEventListener("click", decreaseNumber)
-// hungerBtn.addEventListener(`click`, feedPet)
-
 
 let hungerCount = 1;
 let sleepinessCount = 1;
@@ -68,7 +28,6 @@ function napTime() {
 }
 
 function playWithPet() {
-    // buttonClicked *= 0;
     boredomCount =1;
     boredomLevel.innerHTML = `Boredom: ${boredomCount}`
 }
@@ -121,35 +80,3 @@ function countSleepinessUp () {
         ageCount.innerHTML = `Age: ${ageCounter*=0}`
     }
 }
-
-// function petStatusUpdate () {
-//     if (sleepinessCount >=5 && sleepinessCount<9) {
-//         petStatus.innerText = `Lucky's Status: Tired`
-//     }
-// }
-
-
-
-// function deathFunction () {
-//     if (hungerCount === 10 || boredomCount === 10 || sleepinessCount === 10){
-//         console.log("pet has died")
-//     } else {
-//         console.log("pet is alive")
-//     }
-// }
-
-// const boredomInterval = setInterval(countBoredomUp, 3000)
-// let bordomeCount =0;
-
-// function countBoredomUp () {
-//     if (bordomeCount>=0){
-//         boredomLevel.innerHTML = `Boredom: ${bordomeCount++}`
-//     } 
-// }
-
-//     // if (buttonClicked >= 0){
-//     //     buttonClicked = buttonClicked++
-//     // } else {
-//     //     buttonClicked = -1
-//     // }
-// }
