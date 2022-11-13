a47e369b5bc25a9e154a2c0366ae1ce8e8258cd8
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-12 23:06:47 -0500

    committing changes, still need to fix Pet Naming Convention

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const lifeOfPet = document.getElementsByClassName("life-of-pet-text")
 const petBio = document.getElementsByClassName("pet-bio")
 const ageCount = document.querySelector(".age-count")
 const petStatus = document.querySelector(".pet-status")
-const petName = document.getElementsByClassName("pet-name-input")
 const boredomLevel = document.querySelector(".boredom-level")
 const hungerLevel = document.querySelector(".hunger-level")
 const sleepinessLevel = document.querySelector(".sleepiness-level")
@@ -16,6 +15,10 @@ let hungerCount = 1;
 let sleepinessCount = 1;
 let boredomCount = 1;
 let ageCounter = 1;
+
+document.getElementsByClassName("submit-button").onclick = function namePet() {
+    document.getElementsByClassName("pet-name-text").innerText = petName;
+}
 
 function feedPet() {
     hungerCount = 1;
@@ -32,7 +35,6 @@ function playWithPet() {
     boredomLevel.innerHTML = `Boredom: ${boredomCount}`
 }
 
-
 const hungerInterval = setInterval(countHungerUp, 2000)
 const boredomInterval = setInterval(countBoredomUp, 1500)
 const sleepyInterval = setInterval(countSleepinessUp, 2500)
@@ -48,7 +50,7 @@ function countHungerUp () {
     if (hungerCount>=1 && hungerCount < 11){
         hungerLevel.innerHTML = `Hunger: ${hungerCount++}`
     } else if (hungerCount > 9){
-        petStatus.innerText = `Lucky's Status: DEAD!`
+        petStatus.innerText = `Pet Status: DEAD!`
         boredomLevel.innerHTML = `Boredom: ${boredomCount*=0}`
         sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount*=0}`
         hungerLevel.innerHTML = `Hunger: ${hungerCount*=0}`
@@ -60,8 +62,7 @@ function countBoredomUp () {
     if (boredomCount>=1 && boredomCount < 11){
         boredomLevel.innerHTML = `Boredom: ${boredomCount++}`
     } else if (boredomCount > 9){
-        petStatus.innerText = `Lucky's Status: DEAD!`
-        ageCounter === 0
+        petStatus.innerText = `Pet Status: DEAD!`
         boredomLevel.innerHTML = `Boredom: ${boredomCount*=0}`
         sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount*=0}`
         hungerLevel.innerHTML = `Hunger: ${hungerCount*=0}`
@@ -73,7 +74,7 @@ function countSleepinessUp () {
     if (sleepinessCount >= 1 && sleepinessCount < 11){
         sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount++}`
     } else if (sleepinessCount > 9){
-        petStatus.innerText = `Lucky's Status: DEAD!`
+        petStatus.innerText = `Pet Status: DEAD!`
         boredomLevel.innerHTML = `Boredom: ${boredomCount*=0}`
         sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount*=0}`
         hungerLevel.innerHTML = `Hunger: ${hungerCount*=0}`
