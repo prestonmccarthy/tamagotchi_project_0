b18ed96dd36fc35adea7e96974b95fe308f2d473
prestonmccarthy <sm.preston.mccarthy@gmail.com>
2022-11-12 20:54:05 -0500

    Pet Status changes when Boredom Hunger or Sleepiness hit 10

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const lifeOfPet = document.getElementsByClassName("life-of-pet-text")
 const petBio = document.getElementsByClassName("pet-bio")
 
 
-const ageCount = document.getElementsByClassName("age-count")
+const ageCount = document.querySelector(".age-count")
 
 
-const petStatus = document.getElementsByClassName("pet-status")
+const petStatus = document.querySelector(".pet-status")
 
 
 const petName = document.getElementsByClassName("pet-name-input")
@@ -52,18 +52,94 @@ const sleepBtn = document.getElementById("nap-button")
 // hungerBtn.addEventListener(`click`, feedPet)
 
 
-let buttonClicked = 0;
+let hungerCount = 1;
+let sleepinessCount = 1;
+let boredomCount = 1;
+let ageCounter = 0;
+
 function feedPet() {
-    buttonClicked *= 0;
-    hungerLevel.innerHTML = `Hunger: ${buttonClicked}`
+    hungerCount *= 0;
+    hungerLevel.innerHTML = `Hunger: ${hungerCount}`
 }
 
 function napTime() {
-    buttonClicked *= 0;
-    sleepinessLevel.innerHTML = `Sleepiness: ${buttonClicked}`
+    sleepinessCount *= 0;
+    sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount}`
 }
 
 function playWithPet() {
-    buttonClicked *= 0;
-    boredomLevel.innerHTML = `Boredom: ${buttonClicked}`
+    // buttonClicked *= 0;
+    boredomCount *=0;
+    boredomLevel.innerHTML = `Boredom: ${boredomCount}`
 }
+
+
+const hungerInterval = setInterval(countHungerUp, 1000)
+
+function countHungerUp () {
+    if (hungerCount>=0 && hungerCount < 11){
+        hungerLevel.innerHTML = `Hunger: ${hungerCount++}`
+    } 
+}
+
+const boredomInterval = setInterval(countBoredomUp, 2500)
+
+function countBoredomUp () {
+    if (boredomCount>=0 && boredomCount < 11){
+        boredomLevel.innerHTML = `Boredom: ${boredomCount++}`
+    } 
+}
+
+const sleepyInterval = setInterval(countSleepinessUp, 500)
+
+function countSleepinessUp () {
+    if (sleepinessCount >= 0 && sleepinessCount < 11){
+        sleepinessLevel.innerHTML = `Sleepiness: ${sleepinessCount++}`
+    } else if (sleepinessCount > 9){
+        petStatus.innerText = `Lucky's Status: DEAD!`
+    }
+}
+
+// function deathAnnouncement () {
+//     if (sleepinessCount > 9 || boredomCount > 9 || hungerCount > 9){
+//         petStatus.innerText = `Lucky's Status: DEAD!`
+//     }
+// }
+
+// function petStatusUpdate () {
+//     if (sleepinessCount >=5 && sleepinessCount<9) {
+//         petStatus.innerText = `Lucky's Status: Tired`
+//     }
+// }
+
+const ageInterval = setInterval(countAgeUp, 5000)
+
+function countAgeUp () {
+    if (ageCounter>=0){
+        ageCount.innerHTML = `Age: ${ageCounter++}`
+    } 
+}
+
+// function deathFunction () {
+//     if (hungerCount === 10 || boredomCount === 10 || sleepinessCount === 10){
+//         console.log("pet has died")
+//     } else {
+//         console.log("pet is alive")
+//     }
+// }
+
+// const boredomInterval = setInterval(countBoredomUp, 3000)
+// let bordomeCount =0;
+
+// function countBoredomUp () {
+//     if (bordomeCount>=0){
+//         boredomLevel.innerHTML = `Boredom: ${bordomeCount++}`
+//     } 
+// }
+
+//     // if (buttonClicked >= 0){
+//     //     buttonClicked = buttonClicked++
+//     // } else {
+//     //     buttonClicked = -1
+//     // }
+// }
